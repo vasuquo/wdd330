e8bd3ba67409e7d22018e50400fb2bb3968d534f
vasuquo <veasuquo@gmail.com>
2025-09-21 21:47:39 +0100

    header, footer, update

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const ulElement = document.querySelector(".product-list");
 
 const dataSource = new ProductData("tents");
 const prouctList = new ProductList("tents", dataSource, ulElement);
 
+loadHeaderFooter();
 prouctList.init();
